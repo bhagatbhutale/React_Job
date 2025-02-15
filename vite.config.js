f1f87da5ceb63b90e83e00832080ca0e5eeeebe5
BhagatBhutale <bhagatbhutale57@gmail.com>
2025-02-15 15:20:30 +0530

    Making a Frontend is Completed

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server : {
-    port : 3000,
-  }
-})
+  plugins: [react(), tailwindcss()],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"], // Custom font
+        serif: ["Merriweather", "serif"],
+      },
+      gridTemplateColumns: {
+      "70/30" : "70% 28%",
+      },
+    },
+  },
+  server: {
+    port: 3000,
+  },
+});
