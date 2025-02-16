c9c236e73b1a70daa21c9f3a455be7f7ffb08ab3
BhagatBhutale <bhagatbhutale57@gmail.com>
2025-02-16 12:49:22 +0530

    adding Link to rendering not refreshing page and maing a Not found Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,28 @@
-import Navbar from "./Components/Navbar"
-import Hero from "./Components/Hero"
-import HomeCards from "./Components/HomeCards";
-import JobListing from "./Components/JobListings";
-import ViewsAllJob from "./Components/ViewsAllJob";
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom";
+// import Navbar from "./Components/Navbar"
+// import Hero from "./Components/Hero"
+// import HomeCards from "./Components/HomeCards";
+// import JobListing from "./Components/JobListings";
+// import ViewsAllJob from "./Components/ViewsAllJob";
+import HomePage from "./Pages/HomePage";
+import MainLayout from "./layouts/MainLayout";
+import JobPage from "./Pages/JobPage";
+import NotFoundPage from "./Pages/NotFoundPage";
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path="/jobs" element={<JobPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Route>
+  )
+);
 
 
 const App = () => {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <HomeCards />
-      <JobListing />
-      <ViewsAllJob />
-
-    
-    </>
-  );
+  return <RouterProvider router={router} />
 }
 
 export default App
---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,17 @@
+import Hero from "../Components/Hero"
+import HomeCard from "../Components/HomeCards";
+import JobListings from "../Components/JobListings";
+import ViewsAllJob from "../Components/ViewsAllJob";
+const HomePage = () => {
+  return (
+  <>
+  <Hero />
+  <HomeCard />
+  <JobListings />
+  <ViewsAllJob />
+  </>
+  )
+}
+
+export default HomePage
+
